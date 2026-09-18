7e42f15715b5836c0785b8dd95fd6ab19cca2650
powerysh-ysh <powerysh@gmail.com>
2026-09-19 04:56:38 +0900

    Create kakao-config.js for Kakao API integration
    Add Kakao JavaScript key configuration for the application.

---
+++ kakao-config.js
@@ -0,0 +1,6 @@
+// 카카오 개발자(https://developers.kakao.com) > 내 애플리케이션 > 앱 키 > JavaScript 키를 붙여넣으세요.
+// 그리고 반드시 [내 애플리케이션 > 플랫폼 > Web]에 이 사이트 도메인을 등록해야 지도가 뜹니다:
+//   https://powerysh-ysh.github.io
+//   http://localhost:8934  (로컬 테스트용)
+export const KAKAO_JS_KEY = "여기에-카카오-JavaScript-키";
+
