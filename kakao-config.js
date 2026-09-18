2b8677f4529d3df28577e4897fb154a6b5651896
powerysh-ysh <powerysh@gmail.com>
2026-09-19 05:14:20 +0900

    Update KAKAO_JS_KEY with actual JavaScript key

--- kakao-config.js
+++ kakao-config.js
@@ -2,5 +2,5 @@
 // 그리고 반드시 [내 애플리케이션 > 플랫폼 > Web]에 이 사이트 도메인을 등록해야 지도가 뜹니다:
 //   https://powerysh-ysh.github.io
 //   http://localhost:8934  (로컬 테스트용)
-export const KAKAO_JS_KEY = "여기에-카카오-JavaScript-키";
+export const KAKAO_JS_KEY = "76c3d26a5c5bfa062610ec853c8265a8";
 
